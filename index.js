2efffc17281c2add7959d685effd46bb1e4c94d1
qazsato <qazsato@gmail.com>
2017-11-18 19:05:09 +0900

    初回読み込み時にkで戻るとエラーとなる不具合修正

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let i = 0;
-const links = Array.from(document.querySelectorAll('.rc .r a:first-child, #navcnt .navend a')); // Google検索結果リンク(前、次含む)
+const links = document.querySelectorAll('.rc .r a:first-child, #navcnt .navend a'); // Google検索結果リンク(前、次含む)
 document.addEventListener('keydown', (e) => {
     if (e.path[0].tagName === 'INPUT' || e.path[0].tagName === 'TEXTAREA') return; // 入力ボックスフォーカス中は何もしない
     if (e.key === 'j') {
@@ -7,6 +7,6 @@ document.addEventListener('keydown', (e) => {
         links[i - 1].focus();
     } else if (e.key === 'k') {
         if (i > 1) i--;
-        links[i - 1].focus();
+        if (i > 0) links[i - 1].focus();
     }
 });
