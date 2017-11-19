d05bebaf4fc79d9f30408638f3412b54abceea36
qazsato <qazsato@gmail.com>
2017-11-20 00:17:25 +0900

    処理整理

--- index.js
+++ index.js
@@ -5,20 +5,23 @@ if (isGoogle()) {
   let i = 0;
   const links = document.querySelectorAll('.rc > .r > a[onmousedown], #navcnt .navend a'); // Google検索結果リンク(前、次含む)
   document.addEventListener('keydown', (e) => {
-    if (e.key !== 'j' && e.key !== 'k' ||
-        e.path[0].tagName === 'INPUT' ||
-        e.path[0].tagName === 'TEXTAREA')  {
-          return; // 入力ボックスフォーカス中は何もしない
+    if (e.path[0].tagName === 'INPUT' || e.path[0].tagName === 'TEXTAREA') return; // 入力ボックスフォーカス中は何もしない
+    if (e.key === 'j') {
+      if (i < links.length) i++;
+      links[i - 1].focus();
+    } else if (e.key === 'k') {
+      if (i > 1) i--;
+      if (i > 0) links[i - 1].focus();
     }
-    if (e.key === 'j' && i < links.length) i++;
-    if (e.key === 'k' && i > 1) i--;
-    if (i > 0) links[i - 1].focus();
   });
 } else {
   const SCROLL_STEP_SIZE = 40;
   document.addEventListener('keydown', (e) => {
     if (e.path[0].tagName === 'INPUT' || e.path[0].tagName === 'TEXTAREA') return; // 入力ボックスフォーカス中は何もしない
-    if (e.key === 'j') scrollBy(0, SCROLL_STEP_SIZE);
-    if (e.key === 'k') scrollBy(0, -1 * SCROLL_STEP_SIZE);
+    if (e.key === 'j') {
+      scrollBy(0, SCROLL_STEP_SIZE);
+    } else if (e.key === 'k') {
+      scrollBy(0, -1 * SCROLL_STEP_SIZE);
+    }
   });
 }
