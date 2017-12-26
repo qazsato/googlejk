e1f6893f89c1c155ccac718ee171b6135765b58c
qazsato <qazsato@gmail.com>
2017-12-27 00:37:54 +0900

    リファクタリング

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+// Google検索結果リンク(前、次含む)
+const links = document.querySelectorAll('.r > a, #navcnt .navend a');
 let i = 0;
-const links = document.querySelectorAll('.r > a, #navcnt .navend a'); // Google検索結果リンク(前、次含む)
 document.addEventListener('keydown', (e) => {
-  if (e.path[0].tagName === 'INPUT' || e.path[0].tagName === 'TEXTAREA') return; // 入力ボックスフォーカス中は何もしない
-  if (e.key === 'j') {
-    if (i < links.length) i++;
+  const key = e.key;
+  const tagName = e.path[0].tagName.toLowerCase();
+  // 入力キーがJK以外と入力ボックスフォーカス中は何もしない
+  if (key !== 'j' && key !== 'k' || tagName === 'input' || tagName === 'textarea') {
+    return;
+  }
+  if (key === 'j' && i < links.length) {
+    i++;
+  } else if (key === 'k' && i > 1) {
+    i--;
+  }
+  if (i > 0) {
     links[i - 1].focus();
-  } else if (e.key === 'k') {
-    if (i > 1) i--;
-    if (i > 0) links[i - 1].focus();
   }
 });
